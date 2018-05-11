7025b62ab79170eec196e997a1ac157885cccde6
Matthew Bentley <matthew@bentley.link>
2018-05-11 16:10:27 -0400

    update for new pydocstyle package

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 
 setup(
     name='pre-commit-mirrors-pep257',
-    version='0.0.1',
-    install_requires=['pep257==0.7.0'],
+    version='0.4.0',
+    install_requires=['pydocstyle==2.1.1'],
 )
